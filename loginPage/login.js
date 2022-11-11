9d3943343c4c69bae0aafc41d8b7b8bcb69ce7f7
Victoria Johnson <johnsonvickie884@gmail.com>
2022-11-11 11:18:58 +0100

    Modified the login page

--- loginPage/login.js
+++ loginPage/login.js
@@ -1,4 +1,20 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+let eye = document.querySelector('#toggle-eye');
+let passwordInput = document.querySelector('#password');
+
+console.log(passwordInput);
+eye.addEventListener('click',()=> {
+    if ( passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eye.className="fa fa-eye-slash"
+    } else {
+        passwordInput.type = "password"
+        eye.className="fa fa-eye"
+
+    }
+    console.log(passwordInput);
+})
+
+
+// image.png
+
+// fa fa-eye-slash
